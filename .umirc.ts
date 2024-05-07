eaf215a2094a3ef969d4a41570bb710ce4d0fc39
0000ofl <1452260067@qq.com>
2024-05-07 18:53:00 +0800

    fix: 名称更改与产物去除

--- .umirc.ts
+++ .umirc.ts
@@ -10,7 +10,7 @@ import { defineConfig } from 'dumi';
 const repo = 'dumi-ui';
 
 export default defineConfig({
-  title: repo,
+  title: 'ow-ui',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo:
